fc2e90827fc61a298894543d311194fefe9d1ba5
Семён <sem.pakhomov2007@yandex.ru>
2023-04-11 00:53:07 +0300

    add full registration page

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,2 +1,3 @@
 from . import locations
 from . import weather
+from . import reporters
--- project_main.py
+++ project_main.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from forms.reporter import RegisterForm
+from data import db_session
+from data.reporters import Reporter
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    db_session.global_init('db/weather.db')
+    app.run(port=8080, host="127.0.0.1", debug=True)
 
 
 @app.route('/')
@@ -21,10 +30,38 @@ def weather_main(region):
 def reporter_enter():
     return render_template('form.html')
 
+
 # Регистрация погодного репортера
-@app.route('/reporter/registration')
+@app.route('/reporter/registration', methods=['GET', 'POST'])
 def reporter_registration():
-    pass
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(Reporter).filter(Reporter.email == form.email.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Пользователь с такой почтой уже зарегистрирован")
+        if db_sess.query(Reporter).filter(Reporter.email == form.email.data).first():
+            return render_template('registration.html', title='Регистрация',
+                                   form=form,
+                                   message="Данный логин уже занят")
+
+        reporter = Reporter(
+            name=form.name.data,
+            surname=form.surname.data,
+            login=form.surname.data,
+            email=form.email.data,
+        )
+        reporter.set_password(form.password.data)
+        db_sess.add(reporter)
+        db_sess.commit()
+        return redirect('/reporter/enter')
+    return render_template('registration.html', title='Регистрация', form=form)
+
 
 # Добавление информации
 @app.route('/reporter/edit')
@@ -33,4 +70,4 @@ def reporter_main():
 
 
 if __name__ == "__main__":
-    app.run(port=8080, host="127.0.0.1")
+    main()
